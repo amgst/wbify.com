78b2ebb141c7f50f430bbdff429d6a751d8f00e0
amgst <121005164+amgst@users.noreply.github.com>
2025-06-09 15:40:17 +0500

    Fix HTML template and button style

--- src/html.js
+++ src/html.js
@@ -3,13 +3,18 @@ import React from "react"
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
-      <head {...props.headComponents}>
+      <head>
         {/* Tailwind CDN */}
         <link href="https://cdn.tailwindcss.com" rel="stylesheet" />
-        {props.preBodyComponents}
+        {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-        {props.body}
+        {props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
         {props.postBodyComponents}
       </body>
     </html>
