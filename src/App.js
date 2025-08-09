5ba148144eccffe6e6cd8fba8aab85a677816193
amgst <amgst99@gmail.com>
2025-08-09 14:54:21 +0500

    blog add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 // Import components
+
 import Home from './components/Home';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
@@ -10,6 +11,8 @@ import About from './components/About';
 import Contact from './components/Contact';
 import ProjectDetails from './components/ProjectDetails';
 import NewsletterManager from './components/NewsletterManager';
+import Blog from './components/Blog';
+import BlogDetail from './components/BlogDetail';
 
 function App() {
   return (
@@ -23,6 +26,8 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/project/:id" element={<ProjectDetails />} />
           <Route path="/admin/newsletter" element={<NewsletterManager />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
         </Routes>
       </div>
     </Router>
